c495a6b7cdb8763daf3467a0be5a0073a6d46586
Igor-prog41 <61401897+Igor-prog41@users.noreply.github.com>
2025-12-03 21:25:04 -0700

    Update settings.py
    change debug to false

--- partsstore/settings.py
+++ partsstore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nzpo+oufwrbwt-a1b81b$_rz+67v-w+=zus32j(tm045xzr7zc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 
 ALLOWED_HOSTS = [
